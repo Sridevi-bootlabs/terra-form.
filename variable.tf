a813edb595455ead83e47340e047205d472a6775
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-08-26 19:10:55 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,65 @@
+variable "subnet_ids" {
+  default = ["subnet-00a771c328055bff2", "subnet-0175eb38117876d57"]
+}
+
+variable "subnet_azs" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ec2_ssh_key" {
+  default = "eks-cluster"
+}
+
+
+variable "role_arn" {
+  default = "arn:aws:iam::779527285137:role/AWSdevopscerttaskrole"
+}
+variable "name" {
+  default = "AWSdevopscerttaskrole"
+}
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_id" {
+  default = "vpc-0b0bca52959e39132"
+}
+
+variable "instance_types" {
+  default = ["t2.micro"]
+}
+
+
+variable "bucket" {
+  description = "bucketname"
+  type        = string
+  default = "my-tf-test-arul"
+}
+variable "acl" {
+  type = string
+  default = "public-read-write"
+}
+variable "aws_lb_name" {
+  type = string
+  default = "test-lb-tf1"
+
+}
+variable "lb_type" {
+  type = string
+  default = "application"
+
+}
+
+variable "vpn_cidr_block" {
+  default = "10.0.0.0/16"
+
+}
+
+variable "public_subnet" {
+  default = "subnet-0175eb38117876d57"
+}
+
+variable "private_subnet" {
+  default = "subnet-00a771c328055bff2"
+}
