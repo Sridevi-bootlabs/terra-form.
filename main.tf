8977573e9defc94bbc5d24e4980bb2a91c937a9b
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-08-30 11:46:16 +0530

    correct

--- main.tf
+++ main.tf
@@ -2,26 +2,18 @@ resource "aws_eks_cluster" "eks_cluster" {
     name = "eks_task"
     role_arn = "arn:aws:iam::779527285137:role/AWSdevopscerttaskrole"
     vpc_config{
-        subnet_ids = aws_subnet.subnet[*].id
+        subnet_ids = var.subnet_ids
     }
 
 
 }
 
-resource "aws_vpc" "vpc"{
-    cidr_block = "10.0.0.0/16"
-}
-resource "aws_subnet" "subnet"{
-    vpc_id=aws_vpc.vpc.id
-    cidr_block ="10.0.1.0/24"
-
-}
 
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "example"
   node_role_arn   = "arn:aws:iam::779527285137:role/AWSdevopscerttaskrole"
-  subnet_ids      = aws_subnet.subnet[*].id
+  subnet_ids      = var.subnet_ids
 
   scaling_config {
     desired_size = 1
@@ -30,6 +22,6 @@ resource "aws_eks_node_group" "example" {
   }
 
   update_config {
-    max_unavailable = 2
+    max_unavailable = 1
   }
 }
